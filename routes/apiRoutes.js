a4dc5bf93e831f6debba333309a3f84600279412
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 15:32:46 -0500

    api routes post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
-const notes = require('../public/index.html');
+const { notes } = require('../public/notes.html');
 
 // GET NOTES
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
 });
+
+router.post('/api/notes', (req,res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNotes(req.body)) {
+        res.status(400).send('The note is not property formatted');
+    } else {
+        const note = createNewNote(req.body, animals);
+        res.json(note);
+    }
+});
+
+module.exports = router;
