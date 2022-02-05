bae0ea2042345fa57405d857b2c85c12f9555c30
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 15:26:41 -0500

    get route in api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,4 +4,8 @@ const notes = require('../public/index.html');
 // GET NOTES
 router.get('/notes', (req, res) => {
     let results = notes;
-})
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
