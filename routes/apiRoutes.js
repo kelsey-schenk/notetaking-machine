8d8b3d0b58bb6d0a227098e5b965dddb15599ebd
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 16:12:50 -0500

    apiroutes changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { filterByQuery, createNewNote, validateNote }; 
 const { notes } = require('../public/notes.html');
 
 // GET NOTES
@@ -14,7 +15,7 @@ router.get('/api/notes', (req, res) => {
 router.post('/api/notes', (req,res) => {
     req.body.id = notes.length.toString();
 
-    if (!validateNotes(req.body)) {
+    if (!validateNote(req.body)) {
         res.status(400).send('The note is not property formatted');
     } else {
         const note = createNewNote(req.body, note);
@@ -24,7 +25,7 @@ router.post('/api/notes', (req,res) => {
 
 router.delete('/api/notes/:id', (req,res) => {
     notes.delete(req.params.id)
-})
+});
 
 
 module.exports = router;
