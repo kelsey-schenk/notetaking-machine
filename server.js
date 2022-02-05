6379fd3245a30086a70afd14aef069fcff3cbf58
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 15:17:11 -0500

    server changes

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
+
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json')
+const apiRoutes = require('./routes/apiRoutes')
+
