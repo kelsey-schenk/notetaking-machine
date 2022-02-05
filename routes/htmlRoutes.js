a24760177ff2c1a05fe7f2d5657a72a06cce33d7
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 15:48:17 -0500

    html route for notes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+const { rmSync } = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+}
+router.get('*', (req, res) => {
+
+})
