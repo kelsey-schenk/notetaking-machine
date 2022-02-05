a884fb0c2509f0d551fe9ec4e5f5b6b40ecaa4f4
kelsey-schenk <kelsey.schenk@att.net>
2022-02-05 15:50:38 -0500

    * html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
-const { rmSync } = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
-}
-router.get('*', (req, res) => {
+});
 
-})
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router
