25a068bf2ca98c55c05f8e9c3a19561ba301facc
kelsey-schenk <kelsey.schenk@att.net>
2022-02-12 11:28:11 -0500

    notes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,7 +15,7 @@ router.post('/notes', (req,res) => {
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not property formatted');
     } else {
-        const note = createNewNote(req.body);
+        const note = createNewNote(req.body, notes);
         res.json(note);
     }
 });
